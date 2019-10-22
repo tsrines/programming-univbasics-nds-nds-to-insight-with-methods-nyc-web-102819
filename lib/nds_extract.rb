4e43e268412b6c5e8b6af39c75ec079f951b23d5
Timothy Rines <tsrines@gmail.com>
2019-10-22 22:52:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,3 +21,14 @@ end
 def gross_for_director(director_data)
 end
 
+def gross_for_director(director_data)
+  total = 0
+  index = 0
+
+  while index < director_data[:movies].length do
+    total += director_data[:movies][index][:worldwide_gross]
+    index += 1
+  end
+
+  total
+end
