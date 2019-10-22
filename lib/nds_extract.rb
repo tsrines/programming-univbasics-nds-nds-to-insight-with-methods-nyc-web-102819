717d260cd400142da74e19d12179715623997eba
Timothy Rines <tsrines@gmail.com>
2019-10-22 22:49:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,8 @@ def directors_totals(nds)
   dir_index = 0
   while dir_index < nds.length do
     director = nds[dir_index]
-    result[]
+    result[director][:name] = gross_for_director(director)
+    dir_index += 1
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
